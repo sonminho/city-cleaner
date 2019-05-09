c8b2ae53e43e1712575762882fad3e8ccd443943
sonminho <smin2846@gmail.com>
2019-05-09 16:13:38 +0900

    [190509] 데이터베이스 스키마 수정

--- autocar.sql
+++ autocar.sql
@@ -1,28 +1,26 @@
-CREATE TABLE BOARD (
-  BOARD_ID NUMBER PRIMARY KEY,
-  TITLE VARCHAR2(256) NOT NULL,
-  WRITER VARCHAR2(50) NOT NULL,
-  PASSWORD VARCHAR2(20) NOT NULL,
-  READ_CNT NUMBER DEFAULT(0),
-  CONTENT CLOB,
-  REG_DATE DATE DEFAULT(SYSDATE),
-  UPDATE_DATE DATE DEFAULT(SYSDATE)
-);
-
-create sequence board_seq;
-
 drop table clean_user;
 CREATE TABLE clean_user (
   userid varchar(20) not null primary key,
+  is_admin number default(0),
   passwd varchar(20) not null,
   email varchar(30) not null,
-  address varchar(50) not null,
+  address varchar(50),
+  ip varchar(30),
   bin number default(0),
+  cap number default(0),
   lat number default(0.0),
   lon number default(0.0),
   phone varchar(30),
   REG_DATE DATE DEFAULT(SYSDATE),
   UPDATE_DATE DATE DEFAULT(SYSDATE)
+);
+
+drop table garbage_collection;
+CREATE TABLE garbage_collection (
+  garbage_no number primary key,
+  userid varchar(20) not null,
+  cap number default(0),
+  empty_date date default(sysdate)
 );
 
 INSERT INTO CLEAN_USER (userid, passwd, email, address, lat, lon, reg_date, update_date) 
@@ -39,4 +37,19 @@ select userid, passwd, email, address, lat, lon, bin, reg_date, update_date
 		from (select row_number() over(order by reg_date asc) as seq, userid, passwd, email, address, lat, lon, bin, reg_date, update_date from clean_user)
 		where seq between 1 and 10;
 
-select row_number() over(order by reg_date asc) as seq, userid, passwd, email, address, lat, lon, bin, reg_date, update_date from clean_user;
+select * from clean_user;
+
+DESC CLEAN_USER;
+
+UPDATE CLEAN_USER SET 
+		 PASSWD='123123',
+		 EMAIL='sim2984@naver.com',
+		 ADDRESS='서울시 강남구',  
+		 BIN=0,
+		 LAT=0.0,
+		 LON=0.12,
+		 PHONE='010-2007-2846' 
+		WHERE USERID='abc';
+    rollback;
+    
+select * from clean_user where bin = 1;
